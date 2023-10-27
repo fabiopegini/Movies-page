babeaca8ccd2540c62e7eb6fbcf5c3b7d8211fbe
Fabio Pegini <fipegini@gmail.com>
2023-10-27 13:47:30 +0200

    Pre-component base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import placeholderimg from './img/placeholder-img.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Rendering
-    </div>
+    <>
+      <header>
+        <h1>DEMO Streaming</h1>
+        <button>Log in</button>
+        <a href="">Start your free trial</a>
+      </header>
+
+      <main>
+        <h2>Popular Titles</h2>
+        <div>
+          <img src={placeholderimg} alt="" />
+          <p>Popular Series</p>
+        </div>
+        <div>
+          <img src={placeholderimg} alt="" />
+          <p>Popular Movies</p>
+        </div>
+      </main>
+
+      <footer>
+        <div>
+          <a href="">Home</a>
+          <a href="">Terms and Conditions</a>
+          <a href="">Privacy Policy</a>
+          <a href="">Collection Statement</a>
+          <a href="">Help</a>
+          <a href="">Manage Account</a>
+        </div>
+        <p>Copyright © 2016 DEMO Streaming. All Rights Reserved.</p>
+        <div>
+          <div>
+            <span>Facebook</span>
+            <span>Twitter</span>
+            <span>Instagram</span>
+          </div>
+          <div>
+            <a href="">App Store</a>
+            <a href="">Google Play</a>
+            <a href="">Microsoft</a>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
